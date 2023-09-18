d028f12f2aa362b41777658bec0ad000193a536e
spelkey-ucd <spelkey@ucdavis.edu>
2023-09-18 11:23:26 -0400

    add plugin option v1.1.0

--- readme.md
+++ readme.md
@@ -3,6 +3,12 @@
 Style shims to make the Wordpress forms plugin, Forminator, consistent with the UC Davis brand theme.
 
 ## How to Use
+
+### Directly Include
 1. Copy `UcdlibForminatorStyles.php` and `dist/css` into your Docker image.
-2. Inherit the `UcdlibForminatorStyles` class - make sure to call `parent::construct()`.
+2. Inherit the `src/UcdlibForminatorStyles` class - make sure to call `parent::construct()`.
 3. Enqueue the css script using [wp_enqueue_style](https://developer.wordpress.org/reference/functions/wp_enqueue_style/)
+
+
+### Install as plugin
+1. Download and place in your plugins directory
---
+++ ucdlib-forminator-styles.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: UC Davis Library Forminator Styles
+ * Description: UC Davis theme style customizations for Forminator forms
+ * Author: UC Davis Library Online Strategy
+ * Version: 1.1.0
+ */
+
+require_once( __DIR__ . '/src/UcdlibForminatorStyles.php' );
+
+class UcdlibForminatorStylesImp extends UcdlibForminatorStyles {
+
+  public function __construct(){
+    parent::__construct();
+    add_action( 'wp_enqueue_scripts', [$this, 'loadStyles'] );
+  }
+
+
+  public function loadStyles(){
+    wp_enqueue_style(
+      'ucdlib-forminator-styles',
+      plugin_dir_url( __FILE__ ) . 'dist/css/ucdlib-forminator-shims-min.css',
+      [],
+      '1.1.0'
+    );
+  }
+
+}
+
+new UcdlibForminatorStylesPlugin();
